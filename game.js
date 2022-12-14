92548508a60f7086c67228b06f40d2bf6471eb41
Bledi <blei.bllaca12@gmail.com>
2022-12-14 20:00:18 +0100

    winner stop game

--- game.js
+++ game.js
@@ -16,10 +16,10 @@ function setup(){
   createCanvas(500,500);
   
   regions.push(new Region('01',320,230))// 01
-  regions.push(new Region('02',230,130))// 02
-  regions.push(new Region('03',140,200))// 03
-  regions.push(new Region('04',210,370))// 04
-  regions.push(new Region('05',310,330))// 05
+  regions.push(new Region('02',230,130,true,20,'2'))// 02
+  regions.push(new Region('03',140,200,true,20,'2'))// 03
+  regions.push(new Region('04',210,370,true,20,'2'))// 04
+  regions.push(new Region('05',310,330,true,20,'2'))// 05
   regions.push(new Region('06',400,310,true,20,'2'))// 06
   regions.push(new Region('07',110,300,true,20,'1'))// 07
 
@@ -30,6 +30,10 @@ function setup(){
 function draw(){
   image(map,0,0,500,500)
   //background(100);
+  if(checkForWinner()){
+    background(255,255,255);
+    regions = [];
+  }
   for(let i=0;i<regions.length;i++){
     regions[i].show();
   }
@@ -38,10 +42,10 @@ function draw(){
     let v1 = createVector(mouseX, mouseY);
     //drawArrow(v0, v1, 'white');
     fill(255)
-    strokeWeight(4);
+    strokeWeight(5);
     stroke(255)
     line(v0.x, v0.y, mouseX, mouseY);
-    
+    strokeWeight(1);
   }
   
   
@@ -104,21 +108,26 @@ function mouseReleased(){
 }
 
 function checkForWinner(){
-  let winner = false;
-  let team = '';
+  let team = regions[0].team;
+  for(let i = 0; i < regions.length; i++){
+    if(regions[i].team != team){
+      return false;
+    }
+  }
+  return true;
 }
 
 // draw an arrow for a vector at a given base position
 function drawArrow(base, vec, myColor) {
-  push();
-  stroke(myColor);
-  strokeWeight(3);
-  fill(myColor);
-  //translate();
-  line(base.x, base.y, vec.x, vec.y);
-  //rotate(vec.heading());
-  //let arrowSize = 15;
-  //translate(vec.mag() - arrowSize, 0);
-  //triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-  pop();
+  // push();
+  // stroke(myColor);
+  // strokeWeight(3);
+  // fill(myColor);
+  // //translate();
+  // line(base.x, base.y, vec.x, vec.y);
+  // //rotate(vec.heading());
+  // //let arrowSize = 15;
+  // //translate(vec.mag() - arrowSize, 0);
+  // //triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+  // pop();
 }
